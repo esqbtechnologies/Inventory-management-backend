13d53a230d6e007f48ca037fad125264ffa3a728
Mridul <mridulgupta12345@gmail.com>
2023-02-23 20:32:21 +0530

    Added end point details

--- tool/views/Verificationviews.py
+++ tool/views/Verificationviews.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from multiprocessing import AuthenticationError
+from django.http import HttpResponse, JsonResponse
+from yaml import serialize
 from ..models.Verificationmodels import verification as Verification
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
+from ..models.Assetmodels import asset as Asset
+from django.core import serializers
+import json
+from rest_framework import status
 # API to Create Verirfication
 
 
@@ -20,3 +25,22 @@ class create_verification(APIView):
         verification.asset = request.data['item_id']
         verification.save()
         return HttpResponse('verification done')
+
+# API To get verification details from QR IDs
+
+
+class get_verification_status(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def post(self, request):
+        qr_id = request.data['Qr_id']
+        asset_data = Asset.objects.get(Qr_id=qr_id)
+        verifydetails = asset_data.verification_set.all()
+        detailArray = []
+        for details in verifydetails:
+            data = serializers.serialize('json', [details,])
+            struct = json.loads(data)
+            data = json.dumps(struct[0])
+            detailArray.append(data)
+        return JsonResponse(detailArray, safe=False, status=status.HTTP_200_OK)
