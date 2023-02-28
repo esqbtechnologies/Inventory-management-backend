2a85822dd08e26ba4b599b4f0166a4c71b9f1696
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-03-01 00:29:29 +0530

    Update Assetviews.py
    Debugged tagging

--- tool/views/Assetviews.py
+++ tool/views/Assetviews.py
@@ -114,14 +114,16 @@ class tagQr(APIView):
         asset_id = request.data['item_code']
         qr_id = request.data['Qr_id']
         try:
+            if Asset.objects.get(Qr_id=qr_id).exists():
+                return JsonResponse({'Response': 'The Qr is already tagged to an asset'}, status=status.HTTP_406_NOT_ACCEPTABLE)
             asset = Asset.objects.get(item_code=asset_id)
             asset.Qr_id = qr_id
             asset.save()
             return JsonResponse({'Response': 'Qr Tagged'}, status=status.HTTP_201_CREATED)
         except:
             return JsonResponse({'error': 'Data not saved'}, status=status.HTTP_400_BAD_REQUEST)
-
-
+        
+        
 # API for getting all Asset
 
 class get_all_asset(APIView):
