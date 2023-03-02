2e093767ee6f89c96e95d82cfd38105d3d3b6edb
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-03-02 19:52:24 +0530

    Update serializers.py

--- tool/serializers.py
+++ tool/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models.Usermodels import User
-
+from .models.Assetmodels import asset
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        
+class Assetserializer(serializers.ModelSerializer):
+    class Meta:
+        model = asset
+        fields = '__all__'
+    
