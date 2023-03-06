f40938adea9856bd92a7c1074c552cf874c5dce1
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-03-06 23:30:16 +0530

    Update Sessionviews.py
    added api to restart latest session

--- tool/views/Sessionviews.py
+++ tool/views/Sessionviews.py
@@ -75,3 +75,35 @@ class get_active_session(APIView):
             return JsonResponse(data, safe=False, status=status.HTTP_200_OK)
         except:
             return JsonResponse({'Result': 'No Active Session exists'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+        
+# API To restart latest session
+class restart_last_session(APIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def get(self, request):
+        token = request.headers['Authorization']
+        token = token[4:]
+        payload = jwt.decode(jwt=token, key=set.SECRET_KEY,
+                             algorithms=['HS256'])
+        user = User.objects.get(email=payload['email'])
+        if user.role == 'General_manager':
+            try:
+                activesession = session.objects.get(isActive=True)
+                return JsonResponse({'Result': 'There exits an already active session'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+            except:
+                try:
+                    data = session.objects.all().order_by('-sessionEndDate')
+                    sess = data[0]
+                    sess.isActive = True
+                    sess.sessionEndDate = None
+                    sess.save()
+                    data = serializers.serialize('json', [sess,])
+                    struct = json.loads(data)
+                    data = json.dumps(struct[0])
+                    return JsonResponse(data, safe=False, status=status.HTTP_200_OK)
+                except:
+                    return JsonResponse({'Result': 'No Session exists to Restart'}, status=status.HTTP_204_NO_CONTENT)
+
+        return JsonResponse({'error': 'user is not authorized to Restart session'}, status=status.HTTP_400_BAD_REQUEST)        
