ed52caa9601fa9d8c2647e210086b78e38a4f5e3
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-03-30 00:04:00 +0530

    Update settings.py
    debugged

--- ivnt_mngmnt/settings.py
+++ ivnt_mngmnt/settings.py
@@ -175,7 +175,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email setup settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dummy007gupta@gmail.com'
+EMAIL_HOST_USER = "dummy007gupta@gmail.com"
+DEFAULT_FROM_EMAIL = "myusername@gmail.com"
+EMAIL_HOST_PASSWORD = "mlizhezvrixvprll"
