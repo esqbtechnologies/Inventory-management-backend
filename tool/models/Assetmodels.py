932223a4eb44212d0b75b8f8c520c51d7f487f43
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-04-13 16:05:07 +0530

    Update Assetmodels.py
    added amount to the asset model

--- tool/models/Assetmodels.py
+++ tool/models/Assetmodels.py
@@ -7,14 +7,14 @@ from .Locationmodels import location
 class asset(models.Model):
     item_code = models.CharField(max_length=100)
     item_name = models.CharField(max_length=200)
-    asset_cls = models.CharField(max_length=200)
-    periodcat = models.CharField(max_length=200)
-    Useful_life = models.CharField(max_length=200)
-    Remain_life = models.CharField(max_length=200)
+    asset_cls = models.CharField(max_length=200,blank = True,null = True)
+    periodcat = models.CharField(max_length=200,blank = True,null = True)
+    Useful_life = models.CharField(max_length=200,blank = True,null = True)
+    Remain_life = models.CharField(max_length=200,blank = True,null = True)
     Warehouse_location = models.ForeignKey(location,blank = True,null = True,on_delete=models.SET_NULL)
     Qr_id = models.CharField(max_length=500, null=True, blank=True)
     is_deleted = models.BooleanField(default = False)
-    
+    amount = models.IntegerField(blank = True, null = True)
     def __str__(self):
         return self.item_code
     # verification_id = models.ForeignKey(
