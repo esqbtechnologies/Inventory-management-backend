6a2b8e3e6138c7986f5f8c0c7ab537d9564dfef1
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-04-15 23:35:52 +0530

    Update settings.py
    removed password

--- ivnt_mngmnt/settings.py
+++ ivnt_mngmnt/settings.py
@@ -180,4 +180,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "esqbtechnologies@gmail.com"
 DEFAULT_FROM_EMAIL = "esqbtechnologies@gmail.com"
-EMAIL_HOST_PASSWORD = "qdnvqhocqysjyhfh"
+EMAIL_HOST_PASSWORD = ""
