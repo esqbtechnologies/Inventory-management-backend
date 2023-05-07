ec5988494730e93300f15f63ee4bc4e3f776860b
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-05-08 03:05:11 +0530

    Update serializers.py
    updated serializers

--- tool/serializers.py
+++ tool/serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from .models.Usermodels import User
 from .models.Assetmodels import asset
+from .models.Locationmodels import location
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        
+
+class locationserializer(serializers.ModelSerializer):
+    class meta:
+        model = location
+        fields = '__all__'
+
 class Assetserializer(serializers.ModelSerializer):
+    Warehouse_location = locationserializer(read_only = True)    
     class Meta:
         model = asset
-        fields = [ 'item_code',
-                 'item_name',
-                 'asset_cls',
-                 'periodcat',
-                 'Useful_life',
-                 'Remain_life',
-                 'Warehouse_location',
-                 'Qr_id',]
+        fields = '__all__'
+              
               
