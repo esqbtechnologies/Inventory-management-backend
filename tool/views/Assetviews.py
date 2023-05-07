136f2d35870941cb1fc700f7765c0eca7d65bb0c
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-05-08 00:39:36 +0530

    Update Assetviews.py
    debugging

--- tool/views/Assetviews.py
+++ tool/views/Assetviews.py
@@ -117,15 +117,15 @@ class fullTextSearch(ListAPIView):
         queryset = Asset.objects.annotate(
             search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by("-rank")
         print(queryset)
-        serialized_data = self.serializer_class.to_representation(queryset)
+#         serialized_data = self.serializer_class.to_representation(queryset)
         print(serialized_data)
 #         outpt = []
-#         for resul in res:
-#             data = serializers.serialize('json', [resul,])
-#             struct = json.loads(data)
-#             data = json.dumps(struct[0])
-#             outpt.append(data)
-        return JsonResponse(serialized_data, safe=False, status=status.HTTP_200_OK)
+        for resul in res:
+            data = serializers.serialize('json', [resul,])
+            struct = json.loads(data)
+            data = json.dumps(struct[0])
+            outpt.append(data)
+        return JsonResponse(output, safe=False, status=status.HTTP_200_OK)
 
 
 # API for Tagging
