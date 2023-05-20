8f7bcb7c5ad32e1df206e3edb9a3fd55f3df82ff
mridul-hub <62867903+mridul-hub@users.noreply.github.com>
2023-05-20 21:49:17 +0530

    Update Assetviews.py
    Debugged untagged asset cnt

--- tool/views/Assetviews.py
+++ tool/views/Assetviews.py
@@ -340,6 +340,11 @@ class cnt_untaged(APIView):
         payload = jwt.decode(jwt=token, key=set.SECRET_KEY,
                              algorithms=['HS256'])
         user = User.objects.get(email=payload['email'])
-        aset = Asset.objects.filter(Warehouse_location = user.location).filter(Qr_id__isnull = True)
-        cnt = len(aset)
-        return JsonResponse({'Untagged':cnt},status=status.HTTP_200_OK)        
+        if user.role == 'General_manager':
+            aset = Asset.objects.filter(Qr_id__isnull = True)
+            cnt = len(aset)
+            return JsonResponse({'Untagged':cnt},status=status.HTTP_200_OK)
+        else:
+            aset = Asset.objects.filter(Warehouse_location = user.location).filter(Qr_id__isnull = True)
+            cnt = len(aset)
+            return JsonResponse({'Untagged':cnt},status=status.HTTP_200_OK)
